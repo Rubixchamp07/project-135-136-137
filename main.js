e5e3ede49c65edeac8184838d4e3651f8d837c6b
Rubixchamp07 <88152130+Rubixchamp07@users.noreply.github.com>
2022-10-18 21:17:40 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,15 +1,17 @@
 status = "";
 objects = [];
 objectDetector = "";
+video = "";
 
 function preload(){
+ 
 }//preload = it keeps it ready to show/html
 
 function setup(){
-    canvas = createCanvas(380, 380);
+    canvas = createCanvas(480, 380);
     canvas.center();
     video = createCapture(VIDEO);
-    video.size(380, 380);
+    video.size(480,380);
     video.hide();
 
 }//setup = the sequence of showing/css
@@ -17,6 +19,7 @@ function setup(){
 function start(){
     objectDetector = ml5.objectDetector('cocossd', modelLoaded);
     document.getElementById('status').innerHTML = 'Status: Object Detecting';
+    object_name = document.getElementById("object_name").value;
 }
 
 function modelLoaded(){
@@ -24,6 +27,43 @@ function modelLoaded(){
     status = true;
 }
 
-function draw(){
-    image(video, 0, 0, 380, 380);
-}//draw = runs code continously/js
+function gotResults(error, results){
+    if (error){
+        console.log(error);
+    }
+    console.log(results);
+    objects = results;
+}
+
+function draw() {
+    image(video, 0, 0, 480, 380);
+        if(status != "")
+        {
+          objectDetector.detect(video, gotResults);
+          for (i = 0; i < objects.length; i++) {
+            document.getElementById("status").innerHTML = "Status : Object Detected";
+            
+            fill("#FF0000");
+            percent = floor(objects[i].confidence * 100);
+            text(objects[i].label + " " + percent + "%", objects[i].x + 15, objects[i].y + 15);
+            noFill();
+            stroke("#FF0000");
+            rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+  
+           
+            if(objects[i].label == object_name)
+            {
+              video.stop();
+              objectDetector.detect(gotResults);
+              document.getElementById("number_of_objects").innerHTML = object_name + " Found";
+              synth = window.speechSynthesis;
+              utterThis = new SpeechSynthesisUtterance(object_name + "Found");
+              synth.speak(utterThis);
+            }
+            else
+            {
+              document.getElementById("number_of_objects").innerHTML = object_name + " Not Found";
+            }          
+           }
+        }
+  }//draw = runs code continously/js
